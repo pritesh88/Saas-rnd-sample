94ebc01a08e0d0414e3fa4163083ec3f897397d4
Pritesh <priteshlad6822@gmail.com>
2026-04-14 01:22:31 +0530

    Added arg DJANGO_SECRET_KEY to dockerfile

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # my-apps
+    "commando",
     "visits",
 ]
 
@@ -142,6 +143,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_BASE_DIR = BASE_DIR / "staticfiles"
+STATICFILES_BASE_DIR.mkdir(exist_ok=True, parents=True)
 STATICFILES_VENDOR_DIR = STATICFILES_BASE_DIR / "vendors"
 
 # source(s) for python manage.py collectstatic 
---
+++ src/commando/management/commands/hello_world.py
@@ -0,0 +1,11 @@
+
+import helpers 
+
+from typing import Any
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args: Any, **options: Any):
+        print("hello world")
