9a2b9822c0efd883cc8e43f6c264bd8b82c93f68
Pritesh <priteshlad6822@gmail.com>
2026-04-21 01:14:53 +0530

    auth service

--- src/auth/views.py
+++ src/auth/views.py
@@ -1,6 +1,10 @@
 from django.contrib.auth import authenticate, login
 from django.shortcuts import render, redirect
 
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
 # create your views here.
 def login_view(request):
     if request.method == "POST":
@@ -14,8 +18,22 @@ def login_view(request):
             login(request, user)
             print("login here!")
             return redirect("/")
-    return render(request, "auth/login.html",{})
+        return render(request, "auth/login.html",{})
 
  
-# def register_view(request):
-#     return render(request, "auth/login.html", {})
+def register_view(request):
+       if request.method == "POST":
+           print(request.POST)
+           username = request.POST.get("username") or None
+           email = request.POST.get("email" ) or None
+           password = request.POST.get("password") or None
+ 
+          #  DJANGO Forms  
+        #    user_exists_qs = User.objects.filter(username__iexact=username).exists()
+        #    email_exists = User.objects.filter(username__iexact=email).exists()
+       try:
+           User.objects.create_user(username, email=email, 
+           password=password)
+       except:
+           pass 
+       return render(request, "auth/register.html", {})
