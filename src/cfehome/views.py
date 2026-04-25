71d37cb022f63e27bec1ffae48ee462652e40585
Pritesh <priteshlad6822@gmail.com>
2026-04-25 14:15:40 +0530

    Staff User Required

--- src/cfehome/views.py
+++ src/cfehome/views.py
@@ -2,7 +2,13 @@ import pathlib
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from django.contrib.admin.views.decorators import staff_member_required
+from django.conf import settings
+
+
 from visits.models import PageVisit
+
+LOGIN_URL = settings.LOGIN_URL
 
 this_dir = pathlib.Path(__file__).resolve().parent
 
@@ -66,6 +72,11 @@ def pw_protected_view(request, *args, **kwargs):
     
     return render(request, "protected/entry.html", {})
 
-@login_required
+@login_required(login_url=LOGIN_URL)
 def user_only_view(request, *args, **kwargs): 
+    # print(request.user.is_staff)
+    return render(request,"protected/user-only.html", {})
+
+@staff_member_required(login_url=LOGIN_URL)
+def staff_only_view(request, *args, **kwargs): 
     return render(request,"protected/user-only.html", {})
