ad7b3473be74613fc0240f9387f7a0791d24f1b8
Pritesh <priteshlad6822@gmail.com>
2026-05-04 18:46:18 +0530

    Custom Permissions for Django Users

--- src/profiles/views.py
+++ src/profiles/views.py
@@ -15,10 +15,22 @@ def profiles_list_view(request):
     return render(request, "profiles/list.html", context)
 
 @login_required
-def profile_detail_view(request, username=None, *args,**kwargs):
-      user = request.user
-    
-      profile_user_obj = get_object_or_404(User, username=username)
+def profile_detail_view(request, username=None, *args,
+**kwargs):
+      user = request.user 
+      print(
+           user.has_perm("subscriptions.basic"),
+           user.has_perm("subscriptions.pro"),
+           user.has_perm("subscriptions.advanced"),
+           user.has_perm("subscriptions.basic_ai")
+            )
+    #   user_groups = user.groups.all()
+    #   print("user_groups", user_groups)
+    #   if user_groups.filter(name__icontains='basic').exists():
+    #        return HttpResponse("Congrats")
+      
+      profile_user_obj = get_object_or_404(User, 
+      username=username)
       is_me = profile_user_obj == user
       context = {
           "object": profile_user_obj,
