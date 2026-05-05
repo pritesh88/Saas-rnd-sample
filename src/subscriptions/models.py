dddb1e76883b67165e0667b6a4040b1cdc6477df
Pritesh <priteshlad6822@gmail.com>
2026-05-05 22:01:23 +0530

    changes

--- src/subscriptions/models.py
+++ src/subscriptions/models.py
@@ -1,12 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import Group
 
 # Create your models here.
-
 class Subscription(models.Model):
     name = models.CharField(max_length=120)
+    groups = models.ManyToManyField(Group)
 
     class Meta:
-        permissions=[
+        permissions = [
              ("advanced" ,"Advanced Perm"), #subscriptions.advanced
              ("pro", "Pro Perm"),       #subscriptions.pro
              ("basic", "Basic Perm"),  # subscriptions.basic
